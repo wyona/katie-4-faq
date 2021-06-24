6b9ce900b66778a91ebea70e38c097fa41aec951
Michael Wechner <michael.wechner@wyona.com>
2021-06-24 15:45:14 +0200

    use flat json

--- clients/katie4faq-js/examples/apache-lucene/Apache Lucene - Lucene™ Tutorials-Dateien/translator.js
+++ clients/katie4faq-js/examples/apache-lucene/Apache Lucene - Lucene™ Tutorials-Dateien/translator.js
@@ -89,9 +89,16 @@ function katie_replace(element, language) {
  */
 function katie_getTextFromTranslationJSON(key, language) {
   console.info("Get value for language '" + language + "' and key '" + key + "' ...");
+
+  deTranslations = {"my_faq":"Meine häufig gestellten Fragen","ask":"Fragen","cancel":"Abbrechen","submit":"Absenden","input_question.placeholder_label":"Eine Frage stellen ...","input_email.placeholder_label":"Ihre E-Mail ...","modal.answer_not_helpful":"Antwort nicht hilfreich?","modal.please_enter_email":"Bitte geben Sie Ihre E-Mail-Adresse ein, und wir werden Ihnen so schnell wie möglich eine bessere Antwort zukommen lassen."};
+
+  enTranslations = {"my_faq":"My Frequently Asked Questions","ask":"Ask","cancel":"Cancel","submit":"Submit","input_question.placeholder_label":"Ask a question ...","input_email.placeholder_label":"Your email ...","modal.answer_not_helpful":"Answer not helpful?","modal.please_enter_email":"Please enter your email and we will send you a better answer as soon as possible."};
+
   if (language == "de") {
-    return key.split('.').reduce((obj, i) => (obj ? obj[i] : null), germanTranslation);
+    return deTranslations[key];
+    //return key.split('.').reduce((obj, i) => (obj ? obj[i] : null), germanTranslation);
   } else {
-    return key.split('.').reduce((obj, i) => (obj ? obj[i] : null), englishTranslation);
+    return enTranslations[key];
+    //return key.split('.').reduce((obj, i) => (obj ? obj[i] : null), englishTranslation);
   }
 }
